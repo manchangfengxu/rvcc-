acad2e41d3d4b7095a59825001f27fed5942bc65
manchangfengxu <1848953550@qq.com>
2024-09-18 09:29:57 +0800

    实现指针

--- rvcc.h
+++ rvcc.h
@@ -88,6 +88,8 @@ typedef enum
   ND_LT,        // <
   ND_LE,        // <=
   ND_ASSIGN,    // 赋值
+  ND_ADDR,      // 取地址 &
+  ND_DEREF,     // 解引用 *
   ND_RETURN,    // return
   ND_IF,        // "if"，条件判断
   ND_FOR,       // "for"，循环
