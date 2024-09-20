049bf895436ce912d850c6507a13bd73111fbca9
manchangfengxu <1848953550@qq.com>
2024-09-21 00:07:55 +0800

    实现多维数组

--- test.sh
+++ test.sh
@@ -42,9 +42,48 @@ assert() {
   fi
 }
 
+# assert 期待值 输入值
+# [1] 返回指定数值
+assert 0 'int main() { return 0; }'
+assert 42 'int main() { return 42; }'
 
+# [2] 支持+ -运算符
+assert 34 'int main() { return 12-34+56; }'
 
+# [3] 支持空格
+assert 41 'int main() { return  12 + 34 - 5 ; }'
 
+# [5] 支持* / ()运算符
+assert 47 'int main() { return 5+6*7; }'
+assert 15 'int main() { return 5*(9-6); }'
+assert 17 'int main() { return 1-8/(2*2)+3*6; }'
+
+# [6] 支持一元运算的+ -
+assert 10 'int main() { return -10+20; }'
+assert 10 'int main() { return - -10; }'
+assert 10 'int main() { return - - +10; }'
+assert 48 'int main() { return ------12*+++++----++++++++++4; }'
+
+# [7] 支持条件运算符
+assert 0 'int main() { return 0==1; }'
+assert 1 'int main() { return 42==42; }'
+assert 1 'int main() { return 0!=1; }'
+assert 0 'int main() { return 42!=42; }'
+assert 1 'int main() { return 0<1; }'
+assert 0 'int main() { return 1<1; }'
+assert 0 'int main() { return 2<1; }'
+assert 1 'int main() { return 0<=1; }'
+assert 1 'int main() { return 1<=1; }'
+assert 0 'int main() { return 2<=1; }'
+assert 1 'int main() { return 1>0; }'
+assert 0 'int main() { return 1>1; }'
+assert 0 'int main() { return 1>2; }'
+assert 1 'int main() { return 1>=0; }'
+assert 1 'int main() { return 1>=1; }'
+assert 0 'int main() { return 1>=2; }'
+assert 1 'int main() { return 5==2+3; }'
+assert 0 'int main() { return 6==4+3; }'
+assert 1 'int main() { return 0*9+5*2==4+4*(6/3)-2; }'
 
 # [9] 支持;分割语句
 assert 3 'int main() { 1; 2;return 3; }'
@@ -126,6 +165,14 @@ assert 3 'int main() { int x[2]; int *y=&x; *y=3; return *x; }'
 assert 3 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *x; }'
 assert 4 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *(x+1); }'
 assert 5 'int main() { int x[3]; *x=3; *(x+1)=4; *(x+2)=5; return *(x+2); }'
+
+# [28] 支持多维数组
+assert 0 'int main() { int x[2][3]; int *y=x; *y=0; return **x; }'
+assert 1 'int main() { int x[2][3]; int *y=x; *(y+1)=1; return *(*x+1); }'
+assert 2 'int main() { int x[2][3]; int *y=x; *(y+2)=2; return *(*x+2); }'
+assert 3 'int main() { int x[2][3]; int *y=x; *(y+3)=3; return **(x+1); }'
+assert 4 'int main() { int x[2][3]; int *y=x; *(y+4)=4; return *(*(x+1)+1); }'
+assert 5 'int main() { int x[2][3]; int *y=x; *(y+5)=5; return *(*(x+1)+2); }'
 
 # 如果运行正常未提前退出，程序将显示OK
 echo OK
