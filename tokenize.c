c4a6aea688d48b8a2e6633acbf2b0ca5d2ff8c18
manchangfengxu <manchangfengxu@LAPTOP-UFJ83H0C>
2024-09-28 01:50:58 +0800

    支持union

--- tokenize.c
+++ tokenize.c
@@ -186,7 +186,7 @@ static bool isKeyword(Token *Tok) {
   // 关键字列表
   static char *Kw[] = {"return", "if",  "else",   "for",
                        "while",  "int", "sizeof", "char", 
-                       "struct"};
+                       "struct", "union"};
 
   // 遍历关键字列表匹配
   for (int I = 0; I < sizeof(Kw) / sizeof(*Kw); ++I) {
