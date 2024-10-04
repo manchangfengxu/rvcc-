14ac984bef28ef59307cfd297f12809bebec2630
manchangfengxu <manchangfengxu@LAPTOP-UFJ83H0C>
2024-10-04 17:02:12 +0800

    [58] 支持short类型

--- tokenize.c
+++ tokenize.c
@@ -179,7 +179,7 @@ static bool isKeyword(Token *Tok) {
   // 关键字列表
   static char *Kw[] = {"return", "if",  "else",   "for",
                        "while",  "int", "sizeof", "char", 
-                       "struct", "union", "long"};
+                       "struct", "union", "long", "short"};
 
   // 遍历关键字列表匹配
   for (int I = 0; I < sizeof(Kw) / sizeof(*Kw); ++I) {
