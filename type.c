e06a58355a79852228f526dbe64fb343463802e0
manchangfengxu <manchangfengxu@LAPTOP-UFJ83H0C>
2024-10-16 12:15:14 +0800

    [81] 支持 ! 操作符

--- type.c
+++ type.c
@@ -141,6 +141,10 @@ void addType(Node *Nd) {
   case ND_FUNCALL:
     Nd->Ty = Nd->FuncType->ReturnTy;
     return;
+  //将节点类型设置为int
+  case ND_NOT:
+    Nd->Ty = TyInt;
+    return;
   // 将节点类型设为 变量的类型
   case ND_VAR:
     Nd->Ty = Nd->Var->Ty;
